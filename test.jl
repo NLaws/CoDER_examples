8331d0f1b496cd940e78d2b9d0779d2a60ee2a35
Nick Laws <laws.nick@gmail.com>
2022-01-17 12:07:28 -0700

    Create test.jl

---
+++ test.jl
@@ -0,0 +1,27 @@
+include("CoDER_examples.jl")
+using Test
+using Gurobi
+
+T = 24
+LDFinputs, bigM = get_LDFinputs(T; v_lolim=0.0);
+
+
+## check UL power flow feasibility
+# model = Model(Gurobi.Optimizer)
+# LDF.build_ldf!(model, LDFinputs)
+# optimize!(model)
+
+#= these voltages should be the same after changing Sbase, which they are.
+julia> maximum(sqrt.(value.(model[:vsqrd])))
+1.008374586764746
+
+julia> minimum(sqrt.(value.(model[:vsqrd])))
+0.9845817662226841
+=#
+
+model_linearized = linearized_problem_bess_bigM(Gurobi.Optimizer, T, LDFinputs, bigM);
+
+model_bileveljump = bileveljump_bess_bigM(Gurobi.Optimizer, T, LDFinputs, bigM);
+
+@test objective_value(model_linearized) ≈ objective_value(model_bileveljump)
+
